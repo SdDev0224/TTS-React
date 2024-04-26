b3b25d9e342e741ce6ec4576a7a2dce1303c29d3
Full-Stack Web | AI | Blockchain | Mobile <161324354+SdDev1223@users.noreply.github.com>
2024-04-25 20:40:35 -0700

    Update formatSeasons.js

--- src/utils/formatSeasons.js
+++ src/utils/formatSeasons.js
@@ -2,9 +2,9 @@ export const formatSeasons = (allEpisodes) => {
   const seasons = {};
   allEpisodes.forEach((e) => {
     if (!seasons.hasOwnProperty(`Season ${e.season}`)) {
-      seasons[`Season ${e.season}`] = [];
+      seasons[`Season ${e.season}`] = []
     }
-    seasons[`Season ${e.season}`].push(e);
+    seasons[`Season ${e.season}`].push(e)
   })
   return seasons
 }
